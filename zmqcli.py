ae91abd62431ddf6fb8e85fad8c824a2d77e94cb
Jeremie Le Hen <jeremie@le-hen.org>
2012-07-13 11:15:33 +0200

    [zmqcli] Add a small client to test the daemon.

---
+++ zmqcli.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import zmq
+import random
+import time
+import sys
+
+context = zmq.Context()
+
+# Socket with direct access to the sink: used to syncronize start of batch
+sock = context.socket(zmq.REQ)
+sock.connect(sys.argv[1])
+
+while True:
+	_ = raw_input()
+	sock.send(_)
+	_ = sock.recv()
+	print _
